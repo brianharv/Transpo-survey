c7401a85734a8aecdd60507d396ebfd225113a09
David Cole <davidtcole@yahoo.com>
2020-08-18 11:35:06 -0700

    create the transpo survey project files and folders, and follow along with the lesson.
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
-event.preventDefault();
-})
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
+    event.preventDefault();
+    //let userResponses = [];
+
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      const workTransportationMode = $(this).val();
+      //userResponses.push(workTransportationMode);
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      const funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
+    $('#transportation_survey').hide();
+  });
+});
